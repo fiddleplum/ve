32668fc4b258cd4e6527fb80386c0686182f39dc
Stephen Hurley <stephenhurley@gmail.com>
2017-03-07 17:12:32 -0800

    Better comments for config and stringutil

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -6,58 +6,56 @@
 
 namespace ve
 {
-	// The tree node structure config. File format below.
+	/*! The tree node structure config.
+	Config is a format similar but simpler than JSON. The syntax is as follows.
+	Root = > Value
+	Value = > Dictionary | List | String
+	Dictionary = > '{' (Key ':' Value)* '}'
+	List = > '[' Value* ']'
+	String = > string ended by whitespace, ] (if in array), or } (if in dictionary) or enclosed by consecutive ', `, or " characters
+	Key = > string ended by ':' or whitespace
+	All white space is trimmed unless quoted
+	*/
 	class Config
 	{
 	public:
-		// Default constructor.
+		//! Default constructor.
 		Config();
 
-		// Construct from a file.
+		//! Construct from a file.
 		Config(std::string const & filename);
 
-		// Save a value to a file.
+		//! Save a value to a file.
 		void save(std::string const & filename) const;
 
-		// Return a string of the config.
+		//! Return a string of the config.
 		void toString(std::string & text, size_t tabDepth = 0) const;
 
-		// The possible types of the value.
+		//! The possible types of the value.
 		enum Type { Dictionary, List, String };
 
-		// Access the value at the key.
+		//! Access the value at the key.
 		std::optional<Config const &> operator [] (std::string const & key) const;
 
-		// Access the value at the key.
+		//! Access the value at the key.
 		std::optional<Config &> operator [] (std::string & key);
 
-		// Access the value at the index.
+		//! Access the value at the index.
 		std::optional<Config const &> operator [] (int i) const;
 
-		// Access the value at the index.
+		//! Access the value at the index.
 		std::optional<Config &> operator [] (int i);
 
-		// The type of the value.
+		//! The type of the value.
 		Type type = String;
 
-		// The text of the string, if it is a string.
+		//! The text of the string, if it is a string.
 		std::string text;
 
-		// The children if it is a dictionary or list.
+		//! The children if it is a dictionary or list.
 		std::map<std::string, Config> children;
 
 	private:
 		void parse(std::string const & text, size_t & i);
 	};
 }
-
-/*
-Config is a format similar but simpler than JSON. The syntax is as follows.
-Root = > Value
-Value = > Dictionary | List | String
-Dictionary = > '{' (Key ':' Value)* '}'
-List = > '[' Value* ']'
-String = > string ended by whitespace, ] (if in array), or } (if in dictionary) or enclosed by consecutive ', `, or " characters
-Key = > string ended by ':' or whitespace
-All white space is trimmed unless quoted
-*/
--- src/util/stringutil.hpp
+++ src/util/stringutil.hpp
@@ -7,45 +7,45 @@
 
 namespace ve
 {
-	// Returns the content of a file.
+	//! Returns the content of a file.
 	std::string readFile(std::string const & filename);
 
-	// Writes the contents of a file.
+	//! Writes the contents of a file.
 	void writeFile(std::string const & filename, std::string const & content);
 
-	// Returns true if the subject begins with the part.
+	//! Returns true if the subject begins with the part.
 	bool beginsWith(std::string const & subject, std::string const & part);
 
-	// Returns true if the subject ends with the part.
+	//! Returns true if the subject ends with the part.
 	bool endsWith(std::string const & subject, std::string const & part);
 
-	// Splits a string up by the delimiter string.
+	//! Splits a string up by the delimiter string.
 	std::vector<std::string> split(std::string const & subject, std::string const & delimiter);
 
-	// Trims the string by whitespace (' ', '\t', '\r', '\n') for left or right sides.
+	//! Trims the string by whitespace (' ', '\t', '\r', '\n') for left or right sides.
 	std::string trim(std::string const & subject, bool left = true, bool right = true);
 
-	// Gets the character at the position.
+	//! Gets the character at the position.
 	unsigned int getChar(std::string const & subject, size_t & i);
 
-	// Gets the string specified by the character.
+	//! Gets the string specified by the character.
 	std::string getStringFromChar(unsigned int c);
 
-	// Returns the byte size of the character starting at i.
+	//! Returns the byte size of the character starting at i.
 	size_t getCharSize(std::string const & subject, size_t i);
 
-	// Returns the byte size of the character preceding i.
+	//! Returns the byte size of the character preceding i.
 	size_t getPrevCharSize(std::string const & subject, size_t i);
 
-	// Reads some text until the delimiter.
+	//! Reads some text until the delimiter.
 	std::string readUntil(std::string const & content, size_t & i, std::string const & delimiter);
 
-	// Read some text until any char in the delimiter.
+	//! Read some text until any char in the delimiter.
 	std::string readUntilAny(std::string const & content, size_t & i, std::string const & delimiters);
 
-	// Reads some text until white space (' ', '\t', '\r', '\n').
+	//! Reads some text until white space (' ', '\t', '\r', '\n').
 	void skipWhiteSpace(std::string const & content, size_t & i);
 
-	// Returns true if the next characters are the token.
+	//! Returns true if the next characters are the token.
 	bool isStringNext(std::string const & content, size_t i, std::string const & token);
 }
